0a506507a8150331d6a65f9371424e0c12f1a4fc
912410023 <912410023@o365.tku.edu.tw>
2023-10-26 19:52:23 +0800

    minior mod

--- demo/script_23.js
+++ demo/script_23.js
@@ -7,14 +7,14 @@ function showdemo(week) {
          break;
         
         case 3:
-        
+            p.innerHTML = `<iframe src="./W03_23/index.html" width="100%" height="100%" />`;
          break;
         case 4:
-
+            p.innerHTML = `<iframe src="./W04_23/index.html" width="100%" height="100%" />`;
          break;
         
         case 5:
-        
+            p.innerHTML = `<iframe src="./W05_23/index.html" width="100%" height="100%" />`;
          break;
         case 6:
             p.innerHTML = `<iframe src="./W06_23/index.html" width="100%" height="100%" />`;
