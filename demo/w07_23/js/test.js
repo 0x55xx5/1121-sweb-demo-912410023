bdae10f76351d2fde73ba5d1205cbe83162ce4a9
912410023 <912410023@o365.tku.edu.tw>
2023-10-26 21:45:22 +0800

    tiles

---
+++ demo/w07_23/js/test.js
@@ -0,0 +1,11 @@
+
+const navbar=document.querySelector('.nav');
+console.log(navbar);
+window.addEventListener('scroll',function(){
+    if(this.window.pageYOffset>100){
+        navbar.classList.add('navbar-fixed');
+    }
+    else{
+        navbar.classList.remove('navbar-fixed');   
+    }
+})
