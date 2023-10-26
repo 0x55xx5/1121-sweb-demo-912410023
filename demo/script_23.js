e3d2107522bc6e670b3847dc3db94fcb4cd0dc01
912410023 <912410023@o365.tku.edu.tw>
2023-10-26 19:37:36 +0800

    W07-P1: Show classdemo of week 2 and week 7

---
+++ demo/script_23.js
@@ -0,0 +1,26 @@
+function showdemo(week) {
+    const p = document.querySelector('.show-classdemo');
+    console.log('p', p);
+    switch(week){
+        case 2:
+         p.innerHTML = `<iframe src="./W02_23/index.html" width="100%" height="100%" />`;
+         break;
+        
+        case 3:
+        
+         break;
+        case 4:
+
+         break;
+        
+        case 5:
+        
+         break;
+        case 6:
+            p.innerHTML = `<iframe src="./W06_23/index.html" width="100%" height="100%" />`;
+         break;        
+        case 7:
+        
+         break;
+    }
+}
