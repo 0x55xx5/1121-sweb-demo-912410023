859323bf017c03b5af39f5dc7155eb383aad72d6
0x55xx5 <asd@easd.com>
2023-10-27 22:07:43 +0800

    update link

--- js/script_23.js
+++ js/script_23.js
@@ -19,8 +19,11 @@ function showdemo(week) {
         case 6:
             p.innerHTML = `<iframe src="./W06_23/index.html" width="100%" height="100%" />`;
          break;        
-        case 7:
-            p.innerHTML = `<iframe src="./index.html" width="100%" height="100%" />`;
+         case 7:
+            p.innerHTML = `<iframe src="./w07_23/index.html" width="100%" height="100%" />`;
+         break;        
+        case 8:
+            p.innerHTML = `<iframe src="../demo/index.html" width="100%" height="100%" />`;
          break;
     }
 }
